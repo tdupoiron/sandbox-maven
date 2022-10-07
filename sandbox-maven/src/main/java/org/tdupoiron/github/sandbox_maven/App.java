241bf3c3f5f091ef1361ca19ab3d2fe46c9eaa2b
Thomas Dupoiron <tdupoiron@github.com>
2022-10-07 15:22:54 +0200

    Inital commit

---
+++ sandbox-maven/src/main/java/org/tdupoiron/github/sandbox_maven/App.java
@@ -0,0 +1,13 @@
+package org.tdupoiron.github.sandbox_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
